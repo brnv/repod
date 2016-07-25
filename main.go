b234b8904675fe403da264cb444fb20e7bc1b478
Alexey Baranov <a.baranov@office.ngs.ru>
2016-07-25 14:40:52 +0700

    add gin handlers

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	docopt "github.com/docopt/docopt-go"
+	"github.com/gin-gonic/gin"
+)
 
+var version = "1.0"
+
+var usage = `repod - daemon to manage packages repository stored on host.
+
+Required. TODO.
+
+Usage:
+	repod -h | --help
+	repod [--listen-address=<url>] [--repository-dir=<path>]
+
+Options:
+    -h --help                   Show this help.
+    --listen-address=<url>      Address to listen requests to.
+                                 [default: :6333]
+    --repository-dir=<path>     Host filesystem directory with repositories.
+                                 [default: /srv/http]
+`
+
+func main() {
+	args, err := docopt.Parse(usage, nil, true, "repod "+version, false)
+	if err != nil {
+		panic(err)
+	}
+
+	api := API{
+		RepositoryDir: args["--repository-dir"].(string),
+	}
+
+	router := gin.New()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	v1 := router.Group("/v1/")
+	v1.GET("/", api.HandleListRepositories)
+	v1.GET("/:repository/", api.HandleListEpochs)
+	v1.GET("/:repository/:epoch/", api.HandleListPackages)
+	v1.POST("/:repository/:epoch/", api.HandlePackageAdd)
+	v1.GET("/:repository/:epoch/:package", api.HandlePackageDescribe)
+	v1.DELETE("/:repository/:epoch/:package", api.HandlePackageDelete)
+	v1.POST("/:repository/:epoch/:package", api.HandlePackageEdit)
+
+	router.Run(args["--listen-address"].(string))
 }
